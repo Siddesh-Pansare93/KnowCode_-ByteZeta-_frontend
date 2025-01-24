ffba16599c6224d6dd2a319677a718e27d24ca59
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-24 20:21:46 +0530

    refactor googleOAuth to register user upon successful sign-in with Google

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,18 @@
-  import * as Linking from 'expo-linking'
+import { registerUser } from '@/api';
+import * as Linking from 'expo-linking'
 
 
-  export const googleOAuth = async (startOAuthFlow: any) => {
-      try {
-        const { createdSessionId, setActive, signUp } = await startOAuthFlow({
-          redirectUrl: Linking.createURL("/(root)/(tabs)/home"),
-        });
-    
-        if (createdSessionId) {
-          if (setActive) {
-            await setActive({ session: createdSessionId });
-    
-          //   if (signUp.createdUserId) {
+export const googleOAuth = async (startOAuthFlow: any) => {
+  try {
+    const { createdSessionId, setActive, signUp } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(onboarding)/(tabs)/UserDataform"),
+    });
+
+    if (createdSessionId) {
+      if (setActive) {
+        await setActive({ session: createdSessionId });
+
+        if (signUp.createdUserId) {
           //     await fetchAPI("/(api)/user", {
           //       method: "POST",
           //       body: JSON.stringify({
@@ -20,26 +21,35 @@
           //         clerkId: signUp.createdUserId,
           //       }),
           //     });
-          //   }
-    
-            return {
-              success: true,
-              code: "success",
-              message: "You have successfully signed in with Google",
-            };
-          }
+
+          console.log(signUp)
+          const response = await registerUser(
+            {
+              name: `${signUp.firstName} ${signUp.lastName}`,
+              email: signUp.emailAddress,
+              clerkId: signUp.createdUserId,
+            }
+          );
         }
-    
+
         return {
-          success: false,
-          message: "An error occurred while signing in with Google",
-        };
-      } catch (err: any) {
-        console.error(err);
-        return {
-          success: false,
-          code: err.code,
-          message: err?.errors[0]?.longMessage,
+          success: true,
+          code: "success",
+          message: "You have successfully signed in with Google",
         };
       }
+    }
+
+    return {
+      success: false,
+      message: "An error occurred while signing in with Google",
     };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      success: false,
+      code: err.code,
+      message: err?.errors[0]?.longMessage,
+    };
+  }
+};
