17d8898e26038c017a4374d0e7b17f726d54cb78
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-24 16:32:06 +0530

    Initial setup for app

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,14 +1,21 @@
 import { View, Text } from 'react-native'
+<<<<<<< HEAD
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import ImageScanner from '@/components/ImageScanner'
 import { registerUser } from "@/api"
 
+=======
+import React from 'react'
+import { useSelector } from 'react-redux'
+import ImageScanner from '@/components/ImageScanner'
+>>>>>>> 289325c (Initial setup for app)
 
 const home = () => {
   // console.log(state)
   const age = useSelector((state : any ) => state.user.userData.age)
   console.log(age)
+<<<<<<< HEAD
 
   useEffect(()=>{
 
@@ -19,6 +26,8 @@ const home = () => {
     
     fetchData()
   } , [])
+=======
+>>>>>>> 289325c (Initial setup for app)
   return (
     <View>
       <Text>home</Text>
